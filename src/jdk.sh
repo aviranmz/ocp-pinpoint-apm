16b45228b9f47d641f0736fb50d5df3e523f1035
aviranmz <34619740+aviranmz@users.noreply.github.com>
2019-02-23 20:14:50 +0200

    Update jdk.sh

--- src/jdk.sh
+++ src/jdk.sh
@@ -1,5 +1,6 @@
 export JAVA_6_HOME=/usr/java/jdk1.6.0_45
-export JAVA_7_HOME=/usr/java/jdk1.7.0_79
-export JAVA_8_HOME=/usr/java/jdk1.8.0_201
-export JAVA_9_HOME=/usr/java/jdk-9.0.4
-JAVA_HOME=/usr/java/jdk1.8.0_201
+export JAVA_7_HOME=/usr/java/jdk1.7.0_79-amd64
+export JAVA_8_HOME=/usr/java/jdk1.8.0_201-amd64
+export JAVA_9_HOME/usr/java/jdk-9.0.4-amd64
+JAVA_HOME=/usr/java/jdk1.8.0_201-amd64
+
